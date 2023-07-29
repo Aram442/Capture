b050318c7fdf83a9b92b84e037390a1d91d9d580
Aram Ezalddin <etec02m210260@epu.edu.iq>
2023-07-29 18:54:06 +0300

    4. Faq Section

---
+++ catpure/src/components/FaqSection.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any questions?<span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
+            voluptatem cum labore officiis commodi doloribus numquam magni, rem
+            atque molestiae eius aliquid, quod dolorum dicta quo quas nam!
+            Provident?
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
+            voluptatem cum labore officiis commodi doloribus numquam magni, rem
+            atque molestiae eius aliquid, quod dolorum dicta quo quas nam!
+            Provident?
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>  diferrent payment methods </h4>
+        <div className="answer">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
+            voluptatem cum labore officiis commodi doloribus numquam magni, rem
+            atque molestiae eius aliquid, quod dolorum dicta quo quas nam!
+            Provident?
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4> what product do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
+            voluptatem cum labore officiis commodi doloribus numquam magni, rem
+            atque molestiae eius aliquid, quod dolorum dicta quo quas nam!
+            Provident?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
