5f2da2de4e86f16a4305f118fd3695ec0d90b0b4
Aram Ezalddin <etec02m210260@epu.edu.iq>
2023-07-30 11:41:44 +0300

    5.intro to Styled Components

--- catpure/src/App.js
+++ catpure/src/App.js
@@ -1,9 +1,11 @@
+//Import Style
+import GolobalStyle from "./components/GlobalStyle"
 //Import Pages
 import AboutUs from "./pages/AboutUs";
-
 function App() {
 return (
   <div className="App">
+    <GolobalStyle/>
     <AboutUs/>
   </div>
 )
