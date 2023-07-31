87da7a5ff67c85a082d8f37b1819c59a7c232ed9
Aram Ezalddin <etec02m210260@epu.edu.iq>
2023-07-31 12:10:38 +0300

    9. React Router

--- catpure/src/App.js
+++ catpure/src/App.js
@@ -1,14 +1,24 @@
 //Import Style
-import GolobalStyle from "./components/GlobalStyle"
+import GolobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
-function App() {
-return (
-  <div className="App">
-    <GolobalStyle/>
-    <AboutUs/>
-  </div>
-)
-}
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import Nav from "./components/Nav";
+//Router
+import { Switch, Route, Routes } from "react-router-dom";
 
+function App() {
+  return (
+    <div className="App">
+     <GolobalStyle /> 
+      <Nav />
+      <Routes>
+        <Route path="/" exact element={<AboutUs/>} />
+        <Route path="/work" exact element={<OurWork/>} />
+        <Route path="/contact" exact element={<ContactUs/>} />
+      </Routes>
+    </div>
+  );
+}
 export default App;
