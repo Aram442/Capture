4c8e8886f69e51e1e29506dfa85b11f60ce2dbf6
Aram Ezalddin <etec02m210260@epu.edu.iq>
2023-08-02 15:57:49 +0300

    14. About Animation

--- capture/src/components/AboutSection.js
+++ capture/src/components/AboutSection.js
@@ -3,34 +3,35 @@ import home1 from "../img/home1.png";
 // Framer Motion
 import { motion } from "framer-motion";
 import { About, Description, Image, Hide } from "../styles";
+import { titleAnim } from "../animation";
+import { fade } from "../animation";
+import { photoAnim } from "../animation";
 
 const AboutSection = () => {
   return (
     <About>
       <Description>
-        <motion.div
-          
-        >
+        <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. we
           have professionals with amazing skills
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
     </About>
   );
