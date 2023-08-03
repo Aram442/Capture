0d7b6cd43921618efb9fd591f96e3e3872208d85
Aram Ezalddin <etec02m210260@epu.edu.iq>
2023-08-03 16:38:38 +0300

    17. Reusable Component

--- capture/src/components/FaqSection.js
+++ capture/src/components/FaqSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Toggle from "./Toggle";
 //Import Styled
 import styled from "styled-components";
 import { About } from "../styles";
@@ -8,19 +9,22 @@ const FaqSection = () => {
       <h2>
         Any questions?<span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How do I start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
-            voluptatem cum labore officiis commodi doloribus numquam magni, rem
-            atque molestiae eius aliquid, quod dolorum dicta quo quas nam!
-            Provident?
-          </p>
+      <Toggle>
+        <div className="question">
+          <h4>How do I start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
+              voluptatem cum labore officiis commodi doloribus numquam magni,
+              rem atque molestiae eius aliquid, quod dolorum dicta quo quas nam!
+              Provident?
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
+      </Toggle>
 
       <div className="question">
         <h4>Daily Schedule</h4>
