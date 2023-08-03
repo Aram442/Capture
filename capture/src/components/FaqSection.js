1e1929ace0114c96618ad0f1196d7e24c0f6b502
Aram Ezalddin <etec02m210260@epu.edu.iq>
2023-08-03 17:45:51 +0300

    18. Finishing FAQ Animation

--- capture/src/components/FaqSection.js
+++ capture/src/components/FaqSection.js
@@ -2,19 +2,19 @@ import React from "react";
 import Toggle from "./Toggle";
 //Import Styled
 import styled from "styled-components";
+import { motion, LayoutGroup } from "framer-motion";
 import { About } from "../styles";
+
 const FaqSection = () => {
   return (
     <Faq>
       <h2>
         Any questions?<span>FAQ</span>
       </h2>
-      <Toggle>
-        <div className="question">
-          <h4>How do I start?</h4>
+      <LayoutGroup>
+        <Toggle title={"How Do I Start?"}>
           <div className="answer">
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
-
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
               voluptatem cum labore officiis commodi doloribus numquam magni,
@@ -22,51 +22,41 @@ const FaqSection = () => {
               Provident?
             </p>
           </div>
-          <div className="faq-line"></div>
-        </div>
-      </Toggle>
-
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
-            voluptatem cum labore officiis commodi doloribus numquam magni, rem
-            atque molestiae eius aliquid, quod dolorum dicta quo quas nam!
-            Provident?
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-
-      <div className="question">
-        <h4> diferrent payment methods </h4>
-        <div className="answer">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
-            voluptatem cum labore officiis commodi doloribus numquam magni, rem
-            atque molestiae eius aliquid, quod dolorum dicta quo quas nam!
-            Provident?
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-
-      <div className="question">
-        <h4> what product do you offer?</h4>
-        <div className="answer">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
-            voluptatem cum labore officiis commodi doloribus numquam magni, rem
-            atque molestiae eius aliquid, quod dolorum dicta quo quas nam!
-            Provident?
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
+              voluptatem cum labore officiis commodi doloribus numquam magni,
+              rem atque molestiae eius aliquid, quod dolorum dicta quo quas nam!
+              Provident?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title={"Diferrent Payment Methods"}>
+          <div className="answer">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
+              voluptatem cum labore officiis commodi doloribus numquam magni,
+              rem atque molestiae eius aliquid, quod dolorum dicta quo quas nam!
+              Provident?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title={"What Product Do You Offer?"}>
+          <div className="answer">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fuga
+              voluptatem cum labore officiis commodi doloribus numquam magni,
+              rem atque molestiae eius aliquid, quod dolorum dicta quo quas nam!
+              Provident?
+            </p>
+          </div>
+        </Toggle>
+        </LayoutGroup>
     </Faq>
   );
 };
